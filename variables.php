b9e6d64a3a323f7fad0a1744892cabc0aee150ee
ARIFANDER TENOUYE <arifandertenouye1@gmail.com>
2023-03-25 06:18:20 +0900

    tambah variables.php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+    // contoh penulisan variables di dalam file PHP
+
+    $title = "SaCode";
+    $tag_line = "Papua Coding Shool";
+    $tahun = "2022";
+
+    // menampilkan data dalam variabel PHP
+
+    echo "$title";
+    echo "<br>";
+    echo "$tag_line";
+    echo "<br>";
+    echo "$tahun";
+?>
