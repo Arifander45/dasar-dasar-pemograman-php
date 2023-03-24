0c736e923bfada27952755c361e33dd83837d22c
ARIFANDER TENOUYE <arifandertenouye1@gmail.com>
2023-03-25 06:46:48 +0900

    tambah echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,11 @@
+<?php
+
+    // contoh penggunaan echo
+    echo "<h1>Papua Tech Komunity</h1>";
+    echo "<p>Berbagi dan Belajar di Komunitas.</p>";
+
+    // contoh penggunaan print
+    echo "<h1>Papua Coding School</h1>";
+    echo "<p>Belajar Coding dari dasar mengikuti kursus.</p>";
+
+?>
