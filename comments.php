d76404e658e44a4c5f61be041810fb2ba70d4467
ARIFANDER TENOUYE <arifandertenouye1@gmail.com>
2023-03-25 05:47:40 +0900

    tambah comments & syntax

---
+++ comments.php
@@ -0,0 +1,15 @@
+<?php
+
+    // contoh komentar single-line di PHP
+
+    # ini juga contoh komentar single-line di PHP
+
+    /*
+        kalau yang merupakan contoh komentar multiple-lines 
+        yang bisa diterapkan dalam sebuah file PHP
+    */
+
+    echo "PHP Comments";
+
+
+?>
